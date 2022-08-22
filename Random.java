8c8fd8430ccbe921bccacada6b1edd86d5e10b88
Toni <60268528+Tmax-GH@users.noreply.github.com>
2022-08-22 14:12:51 +0200

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+public class RandomNumberGenerator
+{
+ public static void main(String args[])
+ {
+	 //Generating random numbers
+	 System.out.println("First Random Number: " + Math.random());
+	 System.out.println("Second Random Number: " + Math.random());
+	 System.out.println("Third Random Number: " + Math.random());
+	 
+ }
+}
